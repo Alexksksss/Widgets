19e70c85bcb8575ee3b490638fa8b774a32c39cb
Alexksksss <sasha.litvak02@mail.ru>
2023-05-01 17:24:25 +0700

    Выполнено

--- mainwindow.h
+++ mainwindow.h
@@ -1,22 +1,50 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include <QtGui>
+#include <QWidget>
+#include <QLabel>
+#include <QFrame>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QValidator>
 #include <QMainWindow>
 
 namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
+
+class MainWindow:public QWidget // класс окна
 {
-    Q_OBJECT
-
+    Q_OBJECT // макрос Qt, обеспечивающий корректное создание сигналов и слотов
+protected:
+    QTextCodec *codec;
+    QFrame *frame; // рамка
+    QLabel *inputLabel; // метка ввода
+    QLineEdit *inputEdit; // строчный редактор ввода
+    QLabel *outputLabel; // метка вывода
+    QLineEdit *outputEdit; // строчный редактор вывода
+    QPushButton *nextButton; // кнопка Следующее
+    QPushButton *exitButton; // кнопка Выход
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
-
-private:
-    Ui::MainWindow *ui;
+    MainWindow(QWidget *parent = 0); // конструктор
+public slots:
+    void begin(); // метод начальной настройки интерфейса
+    void calc(); // метод реализации вычислений
 };
 
+
+class StrValidator:public QValidator // класс компонента проверки ввода
+{
+public:
+    StrValidator(QObject *parent):QValidator(parent){}
+    virtual State validate(QString &str,int &pos)const
+    { return Acceptable;} // метод всегда принимает вводимую строку
+
+};
+
+
 #endif // MAINWINDOW_H
+
+
+
