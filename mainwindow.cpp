e814937cc27f89d48118bf4770249e79e1470ce3
Alexksksss <sasha.litvak02@mail.ru>
2023-05-07 15:00:26 +0700

    Выполнено

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,42 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QVBoxLayout>
+#include <QPushButton>
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-}
 
-MainWindow::~MainWindow()
+MainWindow::MainWindow(QWidget *parent):QWidget(parent)
 {
-    delete ui;
+    codec = QTextCodec::codecForName("UTF-8");//кодек для поддержки русского
+    this->setWindowTitle(codec->toUnicode("Счетчик"));//установка заголовка
+
+    label1 = new QLabel(codec->toUnicode("Cчет по 1"),this);
+    label2 = new QLabel(codec->toUnicode("Cчет по 5"),this);
+
+    edit1 = new Counter("0",this);
+    edit2 = new Counter("0",this);
+
+    calcbutton=new QPushButton("+1",this);
+    exitbutton=new QPushButton(codec->toUnicode("Выход"),this);
+
+    QHBoxLayout *layout1 = new QHBoxLayout();
+    layout1->addWidget(label1);
+    layout1->addWidget(label2);
+
+    QHBoxLayout *layout2 = new QHBoxLayout();
+    layout2->addWidget(edit1);
+    layout2->addWidget(edit2);
+
+    QHBoxLayout *layout3 = new QHBoxLayout();
+    layout3->addWidget(calcbutton);
+    layout3->addWidget(exitbutton);
+
+    QVBoxLayout *layout4 = new QVBoxLayout(this);
+    layout4->addLayout(layout1);
+    layout4->addLayout(layout2);
+    layout4->addLayout(layout3);
+
+    // связь сигнала нажатия кнопки и слота закрытия окна
+    connect(calcbutton,SIGNAL(clicked(bool)), edit1,SLOT(add_one()));
+    connect(edit1,SIGNAL(tick_signal()), edit2,SLOT(add_one()));
+    connect(exitbutton,SIGNAL(clicked(bool)), this,SLOT(close()));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,21 +2,49 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtGui>
+#include <QWidget>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
 
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 
-class MainWindow : public QMainWindow
+class Counter:public QLineEdit
 {
     Q_OBJECT
-
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
-
-private:
-    Ui::MainWindow *ui;
+    Counter(const QString & contents, QWidget *parent=0):
+    QLineEdit(contents,parent){}
+signals:
+    void tick_signal();
+public slots:
+    void add_one()
+    {
+        QString str=text();
+        int r=str.toInt();
+        if (r!=0 && r%5 ==0)
+            emit tick_signal();
+        r++;
+        str.setNum(r);
+        setText(str);
+    }
 };
 
+class MainWindow: public QWidget{
+    Q_OBJECT
+protected:
+    QTextCodec *codec;
+    QLabel *label1,*label2;
+    Counter *edit1,*edit2;
+    QPushButton *calcbutton;
+    QPushButton *exitbutton;
+public:
+    MainWindow(QWidget *parent = 0);
+};
 #endif // MAINWINDOW_H
+
+
+
